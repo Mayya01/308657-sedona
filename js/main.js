8fe43d36aa56398ca62702fa55db2be870ef8ee4
Mayya Khmelnitskaya <mayahm01@mail.ru>
2017-05-10 15:11:52 +0300

    Added handlers,map

--- js/main.js
+++ js/main.js
@@ -1,42 +1,44 @@
 'use strict';
-(function(){
+
   var hotelsLink = document.querySelector('.best-offer-hotels');
   var hotelsForm = document.querySelector('.hotels-form');
   var dateOfCheckIn  = hotelsForm.querySelector('input[name=time-of-checkin]');
   var dateOfCheckOut = hotelsForm.querySelector('input[name=time-of-checkout]');
   var button = hotelsForm.querySelector('button[name = results]');
-  var numberOfAdults = hotelsForm.querySelector('.adults');
-  var numberOfChildren = hotelsForm.querySelector('.children');
-  var storage = localStorage.getItem('adults');
+    var iframe = document.querySelector('iframe');
+
 
    hotelsLink.addEventListener('click', function(evt){
        evt.preventDefault();
+     if (hotelsForm.classList.contains('visible')){
+      hotelsForm.classList.remove('visible');
+     } else {
     hotelsForm.classList.add('visible');
-    if (storage){
-      numberOfAdults.value = storage;
-      numberOfChildren.value = storage;
-    }
-
+     }
    });
+
     hotelsLink.addEventListener('keydown', function(evt){
        evt.preventDefault();
-        if(evt.keyCode === 13){
-            hotelsForm.classList.add('visible');
+      if (hotelsForm.classList.contains('visible')){
+        if (evt.keyCode === 27){
+          hotelsForm.classList.remove('visible')
         }
-    })
+      }
+        else {
+          if (evt.keyCode === 13){
+              hotelsForm.classList.add('visible');
+          }
+        }
+    });
 
     button.addEventListener('click', function(evt){
         evt.preventDefault();
         if(!dateOfCheckIn.value || !dateOfCheckOut.value){
           dateOfCheckIn.style.border = '1px solid red';
           dateOfCheckOut.style.border = '1px solid red';
-        } else {
-          localStorage.setItem('adults', numberOfAdults.value);
-          localStorage.setItem('children', numberOfChildren.value);
+          hotelsForm.classList.add('error');
         }
 
-    })
+    });
 
 
-
-})();
